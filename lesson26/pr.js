ba833f4e0fd665be64a58fda4eda228e92e3554d
Arturrr13 <camyrauu911@gmail.com>
2022-05-02 16:39:13 +0300

    add hw26

---
+++ lesson26/pr.js
@@ -0,0 +1,43 @@
+function uName(){
+    const name = prompt('Ваше ім`я');
+    alert('Привіт, ' +name);
+}
+
+function uAge(){
+    const year = 2022;
+    const userage = parseInt(prompt('Рік вашого народження'));
+    const rez = year-userage;
+    alert('Ваш вік: ' +rez);
+}
+
+function square(){
+    const side = parseInt(prompt('Введіть сторону квадрату'));
+    const p = side*4;
+    alert('Периметр: ' +p);
+}
+
+function circle(){
+    const r = parseInt(prompt('Введіть радіус'));
+    const s = 3.14*r**2;
+    alert('Площа: ' +s);
+}
+
+function USDinEUR(){
+    const usd = parseInt(prompt('Введіть $$$-'));
+    eurtousdt=0.95;
+    let eur = usd*eurtousdt;
+    alert(`EUR: ${eur}`);
+}
+
+function lastINfirst(){
+    const num = parseInt(prompt('Введіть пятизначне число'));
+    let last = num%10;
+    let lastdel = (num/10)+(last*10000);
+    alert(Math.floor(`${lastdel}`));
+}
+
+function salary(){
+    const allmoney = parseInt(prompt('Введіть пятизначне число'));
+    let salary = allmoney*0.1+250;
+    alert((`ЗП=== ${salary}`));
+}
