8d0ac600b3cec3c122a436271477d82d392a3d86
Arturrr13 <camyrauu911@gmail.com>
2022-06-21 13:16:14 +0300

    add exam2

--- exam2/assets/js/main.js
+++ exam2/assets/js/main.js
@@ -29,19 +29,114 @@ $(function(){
               spaceBetween: 10,
             },
             "@0.75": {
-              slidesPerView: 2,
+              slidesPerView: 1,
               spaceBetween: 10,
             },
             "@1.00": {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            "@1.50": {
               slidesPerView: 3,
               spaceBetween: 20,
             },
-            "@1.50": {
-              slidesPerView: 4,
-              spaceBetween: 30,
-            },
           },
-          
         });
+
+
+        $('.menuToggle').on('click', function(){
+          $('.burger__nav').toggleClass('active')
+          if($('.burger__nav').hasClass('active')){
+            $('.nav__block').css({display: 'none'})
+            $('.navigation').css({justifyContent: 'flex-end'}).removeClass('scroll')
+            $('.disp__bl').css({
+              height: '100%', width: '100%', background: 'rgba(92, 33, 253, 0.593)', position: 'fixed', top:0, left:0, zIndex: 10, display: 'block'
+            })
+            $('.navigation').removeClass('scroll')
+          } else {
+            $('.nav__block').css({display: 'flex'})
+            $('.navigation').css({justifyContent: 'space-between'}).addClass('scroll')
+            $('.disp__bl').css({display: 'none'})
+          }
+        })
+        $('.menu a, .disp__bl').on('click', function(){
+          $('.disp__bl').css({display: 'none'})
+          $('.burger__nav').toggleClass('active')
+          $('.nav__block').css({display: 'flex'})
+          $('.navigation').css({justifyContent: 'space-between'}).addClass('scroll')
+        })
+        $('.nav__block .anchor, .menu .anchor').on('click', function(e){
+          e.preventDefault();
+          $('html, body').animate({scrollTop:$($(this).attr('href')).offset().top-$('.navigation').height()}, 500)
+        })
+        $(window).on('scroll', function(){
+          $(window).scrollTop() > 0 ? $('.navigation').addClass('scroll') : $('.navigation').removeClass('scroll')
+        })
+
+
+        $('#map__open').on('click', function(e){
+          e.preventDefault()
+          e.stopPropagation()
+          $('#map').html('')
+          openMap()
+        })
+
+
+        $(window).on('resize', function(){
+          if($(window).width() <= 773){
+            $('.get__in__touch--close, .form__wrapper').css({display:'none'})
+            $('.get__in__touch--open').css({display:'block'})
+            $('.get__in__touch').css({height: 175, width: '100%', bottom: 538})
+          } else{
+            $('.get__in__touch--open, .get__in__touch--close').css({display:'none'})
+            $('.get__in__touch').css({height: 450, width: 770, bottom: 399})
+            $('.form__wrapper').css({display:'flex', flexDirection: 'row-reverse'})
+          }
+        })
+
+        $('.get__in__touch--open').on('click', function(){
+          $(this).css({display:'none'})
+          $('.get__in__touch').css({height:610, transition: '1.5s'})
+          $('.get__in__touch--close').css({display:'block'})
+          $('.form__wrapper').css({display:'flex', flexDirection: 'column', opacity: 0}).animate({opacity: 1}, 3500)
+          $('.form').css({marginBottom: 10})
+        })
+
+        $('.get__in__touch--close').on('click', function(){
+          $(this).css({display:'none'})
+          $('.get__in__touch').css({height:175, transition: '1.5s'})
+          $('.get__in__touch--open').css({display:'block'})
+          $('.form__wrapper').css({display:'none'})
+        })
+
+
+        $('#email').on('blur', function(){
+          let email = $(this).val();
+          (email.length > 0 && (email.match(/.+?\@.+/g) || []).length !== 1) ? $('.email__error').css({display: 'block'}).html('Incorrec email!') : $('.email__error').css({display: 'none'})
+        })
+        $('#name').on('blur', function(){
+          let name = $(this).val();
+          (name.length < 1 || name.match(/[0-9]/) !== null) ? $('.name__error').css({display: 'block'}).html('Incorrec name!') : $('.name__error').css({display: 'none'})
+        })
 })
 
+
+function openMap() {
+var myIcon = L.icon({
+  iconUrl: 'assets/leaflet/images/Pin.png',
+
+  iconSize:     [106, 106],
+  iconAnchor:   [22, 94], 
+  popupAnchor:  [-3, -76] 
+});
+
+const map = L.map('map').setView([40.7040524,-73.9242069], 11);
+
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([40.7040524,-73.9242069], {icon: myIcon}).addTo(map)
+      .bindPopup('Hello!!!')
+      .openPopup();
+}
--- exam2/assets/js/news.js
+++ exam2/assets/js/news.js
@@ -2,16 +2,14 @@ fetch('assets/JSON/news.json')
     .then(res => {
         return res.json()
     })
-    .then(res => {
-        news(res)
-    })
+    .then(res => news(res))
 
 function news(news) {
-    let html = '<div class="swiper mySwiper2" style="max-width:1170px; height:584px"><div class="swiper-wrapper">'
+    let html = ''
     news.forEach(el => {
         html += `
-        <div class="swiper-slide" style="margin-right:30px; background: none;width:370px; max-height: 514px; height:100%">
-        <div style="width:370px; max-height: 514px; height:100%;  background:white" class="news__block">
+        <div class="swiper-slide" style="background: none; width:370px; max-height: 514px; height:100%">
+        <div style="width:370px; max-height: 514px; height:100vh; background:white" class="news__block">
             <img src="${el.img}" style="width:100%; max-height:240px"/>
             <p>
                 ${el.title}
@@ -30,6 +28,5 @@ function news(news) {
         </div>
         `
     });
-    html += '</div><div class="swiper-pagination"></div></div><div class="swiper-button-next" id="next__sl--2"></div><div class="swiper-button-prev"></div>'
-    document.getElementById('news').innerHTML = html
+    document.getElementById('a').innerHTML = html
 }
